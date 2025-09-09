6f8ea4473fe2438533148ae7f4c7a8c188ce3dea
R Raksha <154651032+Rakshar30@users.noreply.github.com>
2025-09-09 22:09:15 +0530

    Create Determine The Operating System.java

---
+++ Determine The Operating System.java
@@ -0,0 +1,7 @@
+class Main {
+  public static void main(String[] args) {
+    // get the name of operating system
+    String operatingSystem = System.getProperty("os.name");
+    System.out.println(operatingSystem);
+  }
+}
